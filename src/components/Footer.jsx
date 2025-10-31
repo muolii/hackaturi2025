5306df8b016c7ed285ca2bf94caffe0b4f9f6a03
Ryan Butera <rbutera03@uri.edu>
2025-10-31 09:54:33 -0400

    added MLH code of conduct

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,6 +8,14 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer-bottom">
         <p>&copy; 2025 Hack@URI. All rights reserved.</p>
+        <a 
+          href="https://github.com/MLH/mlh-policies/blob/main/code-of-conduct.md" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          style={{ display: 'block', margin: '8px 0' }}
+        >
+          MLH Code of Conduct
+        </a>
         <p>Made with <FaHeart style={{ color: 'red', verticalAlign: 'middle' }} /> by the Hack@URI Team</p>
       </div>
     </footer>
